13e0750fe2f966a533890699591a36eb227e24f6
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-05-14 01:11:04 +0300

    fix: revert breaking change

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict'
-module.exports = (mos, md) => Object.assign(mos.scope, require('./lib')(md))
+module.exports = plugin
 
-module.exports.attributes = {
-  pkg: require('./package.json'),
+const renderLicense = require('./lib/render-license')
+
+function plugin (markdown) {
+  return {
+    license: () => renderLicense(markdown),
+  }
 }
